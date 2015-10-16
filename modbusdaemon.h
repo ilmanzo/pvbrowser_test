d80f6769fb7f288d166cf83000e91c0af7b690e9
Andrea Manzini <andrea@deb8pvb.manzonet>
2015-10-16 16:35:46 +0200

    first commit

---
+++ modbusdaemon.h
@@ -0,0 +1,13 @@
+// Modbus Signal assignment of modbusdaemon
+#define modbusdaemon_SHARED_MEMORY "/tmp/modbus.shm"
+#define modbusdaemon_MAILBOX "/tmp/modbus.mbx"
+
+#define modbusdaemon_CYCLE1_BASE 0
+#define modbusdaemon_CYCLE1_NUM 2
+#define modbusdaemon_CYCLE1_NUM_BYTES 4
+
+#define modbusdaemon_LIFE_COUNTER_BASE 4
+#define modbusdaemon_READ_ERROR_COUNT_BASE 6
+#define modbusdaemon_WRITE_ERROR_COUNT_BASE 8
+
+#define modbusdaemon_SHARED_MEMORY_SIZE 10
